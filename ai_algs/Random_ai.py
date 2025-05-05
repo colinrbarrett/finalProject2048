739f73d0e354df10c9b6d3327c9f856955651afa
colinrbarrett <colin55b@gmail.com>
2025-05-05 16:25:46 -0400

    fixed random

--- ai_algs/Random_ai.py
+++ ai_algs/Random_ai.py
@@ -4,4 +4,4 @@ from game_2048 import Game2048
 class RandomAgent:
 
     def get_action(self, game_state: Game2048):
-        return random.choice(["Up", "Down", "Left", "Right"])
+        return random.choice(["Up", "Down", "Left", "Right"]), 0, 0, 0, 0
